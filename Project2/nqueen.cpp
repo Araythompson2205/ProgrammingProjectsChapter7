8affbae12b42ea126582aa8ac094ebd1001036c2
Long Duong <longduonglq@gmail.com>
2021-04-12 13:39:13 -0700

    e

--- Project2/nqueen.cpp
+++ Project2/nqueen.cpp
@@ -34,15 +34,15 @@ bool Nqueen::isConflict() {
 
 bool Nqueen::isConflictFast(int y, int x, std::vector<std::vector<int>>& board) {
 	std::array<bool, 8> possibilities;
-	for (int i = 0; i < 8; i++) possibilities[i] = NO_Q;
+	for (int i = 0; i < 8; i++) possibilities[i] = true;
 	possibilities[0] = (isPathFree(x, y, TF(v + 1), TF(v)));
 	possibilities[1] = (isPathFree(x, y, TF(v - 1), TF(v)));
-	possibilities[2] = (isPathFree(x, y, TF(v), TF(v + 1)));
+	//possibilities[2] = (isPathFree(x, y, TF(v), TF(v + 1)));
 	possibilities[3] = (isPathFree(x, y, TF(v), TF(v - 1)));
-	possibilities[4] = (isPathFree(x, y, TF(v + 1), TF(v + 1)));
+	//possibilities[4] = (isPathFree(x, y, TF(v + 1), TF(v + 1)));
 	possibilities[5] = (isPathFree(x, y, TF(v - 1), TF(v - 1)));
 	possibilities[6] = (isPathFree(x, y, TF(v + 1), TF(v - 1)));
-	possibilities[7] = (isPathFree(x, y, TF(v - 1), TF(v + 1)));
+	//possibilities[7] = (isPathFree(x, y, TF(v - 1), TF(v + 1)));
 	return std::any_of(
 		possibilities.begin(), possibilities.end(),
 		[](bool e) {return !e;}
